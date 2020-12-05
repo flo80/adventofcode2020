e5994692a691326fb8d10c3dde9c229d42504194
Florian Lloyd-Pötscher <florian@luke-roberts.com>
2020-12-05 14:25:30 +0100

    enable dynamic loading of days (incl downloading of input files)

--- bin/aoc2020.dart
+++ bin/aoc2020.dart
@@ -1,11 +1,67 @@
-import 'package:aoc2020/day1.dart' as day1;
-import 'package:aoc2020/day2.dart' as day2;
-import 'package:aoc2020/day3.dart' as day3;
-import 'package:aoc2020/day4.dart' as day4;
-import 'package:aoc2020/day5.dart' as day5;
+import 'dart:isolate';
+
+import 'package:aoc2020/common.dart';
 
 void main(List<String> arguments) {
-  print('Advent of Code 2020');
-  day5.partA();
-  day5.partB();
+  print('\x1B[1m\x1B[4mAdvent of Code 2020\x1B[0m');
+  
+  if (arguments.length == 1) {
+    if (arguments[0] == 'all') {
+        runAllDays();
+    }
+    else {
+      final day = int.tryParse(arguments[0]);
+      if (day != null) {
+        print('Will run day $day');
+        runDay(day);
+      } else {
+        print('Please provide day number as first argument');
+      }
+    }
+  } else {
+    final now = DateTime.now();
+    if (now.year == 2020 && now.month == 12) {
+      runDay(now.day);
+    } else {
+      runAllDays();
+    }
+  }
+}
+
+void runAllDays() async {
+  var upto = 25;
+  final now = DateTime.now();
+  if (now.year == 2020 && now.month == 12) {
+    upto = now.day;
+  }
+  for (var day = 1; day <= upto; day++) {
+    await runDay(day);
+  }
+}
+
+void runDay(int day) async {
+  if (!await checkAndDownload(day)) {
+    print(
+        'Could get download input file for day $day, running will probably fail');
+  }
+
+  final uri = Uri(scheme: 'package', path: 'aoc2020/day$day.dart');
+  Isolate iso;
+  final rc = ReceivePort();
+  rc.listen((message) {
+    if (message == null) {
+      rc.close();
+    } else {
+      print(message);
+    }
+  });
+  try {
+    iso = await Isolate.spawnUri(uri, [], rc.sendPort, onExit: rc.sendPort);
+    if (iso == null) {
+      throw Exception('Iso not found');
+    }
+  } catch (e) {
+    print('Could not run Day $day');
+    rc.close();
+  }
 }
--- lib/common.dart
+++ lib/common.dart
@@ -1,8 +1,70 @@
+import 'dart:convert';
+import 'dart:core';
+import 'dart:core' as core;
 import 'dart:io';
+
+import 'dart:isolate';
+
+String path(int day) {
+  return './inputs/day$day';
+}
 
 String loadFile(int day) {
   assert(day > 0 && day < 26);
-  final path = "./inputs/day$day";
-  final file = File(path);
-  return file.readAsStringSync();
+  final file = File(path(day));
+  return file.readAsStringSync().trim();
+}
+
+Future<bool> checkAndDownload(int day) async {
+  final file = File(path(day));
+  if (file.existsSync()) {
+    return Future.value(true);
+  } else {
+    return downloadFile(day);
+  }
+}
+
+Future<bool> downloadFile(int day) async {
+  final cookieF = File('./.cookie');
+  if (!cookieF.existsSync()) {
+    print('Cookie is missing, cannot download file');
+    return false;
+  }
+
+  final cookie = cookieF.readAsStringSync();
+  final url = Uri.parse('https://adventofcode.com/2020/day/$day/input');
+  final request = await HttpClient().getUrl(url);
+  request.cookies.add(Cookie('session', cookie));
+  final response = await request.close();
+  if (response.statusCode == 200) {
+    final outputFile = File(path(day)).openWrite();
+    if (outputFile == null) {
+      print('Could not open file to write');
+      return false;
+    }
+
+    await response.pipe(outputFile);
+    return true;
+  } else {
+    print('Could not download file, status: ${response.statusCode}');
+    response.transform(Utf8Decoder()).listen((event) => print(event));
+    return false;
+  }
+}
+
+enum Part { A, B }
+
+void printHeader(int day, Part part) {
+  final partS = part == Part.A ? 'A' : 'B';
+  print('\n\x1B[1mDay $day - Part $partS\x1B[0m');
+}
+
+SendPort sp;
+
+void print(String message) {
+  if (sp != null) {
+    sp.send(message);
+  } else {
+    core.print(message);
+  }
 }
